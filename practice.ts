081692fa08e71094fdf89507841a1d903df28e8b
balaji-nightwalker <learningaws29@gmail.com>
2026-07-13 15:35:05 +0530

    new fix changes

--- practice.ts
+++ practice.ts
@@ -1,5 +1,7 @@
 console.log("Hello, World!"); 
 console.log("Hello, World!"); 
-console.log("Hello, World!");
+console.log("Welcome to TypeScript playlist");
 const a = 100;
 const b = 100;
+const add = a + b ;
+console.log("The sum of Two Numbers :" + add)
